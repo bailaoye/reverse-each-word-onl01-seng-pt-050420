45b8672e0069a1cb1c283920992bb718cd17586f
Jeff Bai <j.bai@wustl.edu>
2020-05-17 00:06:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   new_sentence = sentence.split(" ")
   reversed_sentence = new_sentence.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  return reversed_sentence.join(" ")
 end
